a405dcb520b75c1b5e1979486effbdebc1581fd6
Adona12 <adonatesfaye99@gmail.com>
2025-05-21 14:40:25 +0000

    update docker compose configuration

--- Production/celery_worker.py
+++ Production/celery_worker.py
@@ -1,9 +1,10 @@
 from celery import Celery
 
 # Celery configuration
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
+CELERY_BROKER_URL = 'amqp://rabbitmq:rabbitmq@localhost:5672/'
 CELERY_RESULT_BACKEND = 'rpc://'
 # Initialize Celery
-celery = Celery('workerA', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery('worker', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 
+import predict_logic
