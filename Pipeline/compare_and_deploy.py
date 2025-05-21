e0c0f8b89d788ef1f72072c72c80f55e3fa9061a
AlbertBS2 <abargallos@gmail.com>
2025-05-21 19:15:30 +0200

    Feat: Add compare_and_deploy.py

---
+++ Pipeline/compare_and_deploy.py
@@ -0,0 +1,47 @@
+# This script compares the challenger and the champion models.
+# If the challenger model obtains an increase in the R2-Score, it is deployed into production
+
+import os
+import shutil
+import joblib
+import pandas as pd
+from sklearn.metrics import r2_score
+
+
+# Paths
+base_dir = os.path.dirname(__file__)
+challenger_model_path = os.path.join(base_dir, '..', 'Development', 'new_model.pkl')
+champion_model_path = os.path.join(base_dir, '..', 'Production', 'best_model.pkl')
+
+# Load models
+challenger_model = joblib.load(challenger_model_path)
+champion_model = joblib.load(champion_model_path)
+
+# Load data
+df = pd.read_csv(data_path)
+
+X = df.copy()
+y = X.pop("stargazers_count")
+
+# Label encoding
+label_encoder = LabelEncoder()
+X_categorical = df.select_dtypes(include=['object']).apply(label_encoder.fit_transform)
+X_numerical = df.select_dtypes(exclude=['object']).values
+X = pd.concat([pd.DataFrame(X_numerical), X_categorical], axis=1).values
+
+# Extract test data
+_, X_test, _, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Evaluate the models
+new_r2 = r2_score(y_test, challenger_model.predict(X_test))
+old_r2 = r2_score(y_test, champion_model.predict(X_test))
+
+print(f"New R²: {new_r2}, Old R²: {old_r2}")
+
+# Compare and deploy
+if new_r2 > old_r2:
+    print("New model is better. Deploying...")
+    shutil.copy(challenger_model_path, champion_model_path)
+
+else:
+    print("Old model is still better. No changes made.")
