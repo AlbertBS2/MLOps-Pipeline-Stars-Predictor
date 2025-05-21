f49f551106906b3d39702f765ace976a02c8277b
AlbertBS2 <abargallos@gmail.com>
2025-05-21 19:53:40 +0200

    Feat: New random forest model

--- Development/training/random_forest.py
+++ Development/training/random_forest.py
@@ -28,7 +28,7 @@ X = pd.concat([pd.DataFrame(X_numerical), X_categorical], axis=1).values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Build and train the model
-model = RandomForestRegressor(random_state=42, max_depth=30)
+model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 # Save the model
