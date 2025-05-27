7dee4d46f11e001e236e619b422aef308251e12c
AlbertBS2 <abargallos@gmail.com>
2025-05-27 22:09:19 +0200

    Fix: output model name

--- Pipeline/compare_and_deploy.py
+++ Pipeline/compare_and_deploy.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 base_dir = os.path.dirname(__file__)
 data_path = os.path.abspath(os.path.join(base_dir, '..', 'Development', 'data', 'repo_data.csv'))
 challenger_model_path = os.path.abspath(os.path.join(base_dir, '..', 'Development', 'models', 'new_model.pkl'))
-champion_model_path = os.path.abspath(os.path.join(base_dir, '..', 'Production', 'new_model.pkl'))
+champion_model_path = os.path.abspath(os.path.join(base_dir, '..', 'Production', 'best_model.pkl'))
 
 # Load models
 challenger_model = joblib.load(challenger_model_path)
